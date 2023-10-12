ecc7644561a1da52774e2821fd8a7017050596f5
RajathJivi <rajatkumar@jivisolutions.com>
2023-10-12 09:15:46 +0530

    test for Azure

--- src/test/java/com/JiviewsAutomation/SystemDefination_TestScript/skillSetUpTestScript.java
+++ src/test/java/com/JiviewsAutomation/SystemDefination_TestScript/skillSetUpTestScript.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.poi.EncryptedDocumentException;
 import org.testng.annotations.Test;
 
+import com.AutomationJiviewsGeneric.BaseClass;
 import com.AutomationJiviewsPOM.EmployeeSetupPage;
 import com.AutomationJiviewsPOM.HomePage;
 import com.AutomationJiviewsPOM.OrganizationUnitDropDown;
@@ -12,85 +13,30 @@ import com.AutomationJiviewsPOM.jiviewsMainMenuItems;
 import com.AutomationJiviews_SystemDefination_POM.SystemDefinationSkillSetupPage;
 
 
-public class skillSetUpTestScript extends com.AutomationJiviewsGeneric.BaseClass{
+public class skillSetUpTestScript extends BaseClass{
 
 	
-	
-	@Test
+	@Test(priority = 1)
 	public void createNewSkill() throws InterruptedException, EncryptedDocumentException, IOException {
 
-		HomePage hp= new HomePage(driver);
+
+		HomePage hp=new HomePage(driver);
 		OrganizationUnitDropDown oudd= new OrganizationUnitDropDown(driver);
 		jiviewsMainMenuItems jmmi= new jiviewsMainMenuItems(driver);
 		EmployeeSetupPage esp=new EmployeeSetupPage(driver);
 		SystemDefinationSkillSetupPage sds=new SystemDefinationSkillSetupPage(driver);
 		hp.setOrgUnit();
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		oudd.setOLMop();
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		jmmi.clickOnSystemDefination();
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		esp.setCreateSkill();
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		sds.setNewSkill();
 
 	}
-	
-
-	@Test
-	public void deActiveSkill() throws InterruptedException, EncryptedDocumentException, IOException {
-		HomePage hp=new HomePage(driver);
-		OrganizationUnitDropDown oudd= new OrganizationUnitDropDown(driver);
-		jiviewsMainMenuItems jmmi= new jiviewsMainMenuItems(driver);
-		EmployeeSetupPage esp=new EmployeeSetupPage(driver);
-		hp.setOrgUnit();
-		Thread.sleep(2000);
-		oudd.setOLMop();
-		Thread.sleep(2000);
-		jmmi.clickOnSystemDefination();
-		Thread.sleep(2000);
-		esp.setCreateSkill();
-		
-		SystemDefinationSkillSetupPage sds=new SystemDefinationSkillSetupPage(driver);
-		Thread.sleep(2000);
-		sds.setDeactiveSkil();
-	}
-	@Test
-	public void setReActiveSkill() throws InterruptedException {
-		HomePage hp=new HomePage(driver);
-		OrganizationUnitDropDown oudd= new OrganizationUnitDropDown(driver);
-		jiviewsMainMenuItems jmmi= new jiviewsMainMenuItems(driver);
-		EmployeeSetupPage esp=new EmployeeSetupPage(driver);
-		SystemDefinationSkillSetupPage sds=new SystemDefinationSkillSetupPage(driver);
-
-		hp.setOrgUnit();
-		Thread.sleep(2000);
-		oudd.setOLMop();
-		Thread.sleep(2000);
-		jmmi.clickOnSystemDefination();
-		Thread.sleep(2000);
-		esp.setCreateSkill();
-		Thread.sleep(2000);
-		sds.setActivateDeactiveSkill();
-	}
-	@Test
-	public void searchColumns() throws InterruptedException, EncryptedDocumentException, IOException {
-		HomePage hp=new HomePage(driver);
-		OrganizationUnitDropDown oudd= new OrganizationUnitDropDown(driver);
-		jiviewsMainMenuItems jmmi= new jiviewsMainMenuItems(driver);
-		EmployeeSetupPage esp=new EmployeeSetupPage(driver);
-		SystemDefinationSkillSetupPage sds=new SystemDefinationSkillSetupPage(driver);
-		hp.setOrgUnit();
-		Thread.sleep(2000);
-		oudd.setOLMop();
-		Thread.sleep(2000);
-		jmmi.clickOnSystemDefination();
-		Thread.sleep(2000);
-		esp.setCreateSkill();
-		Thread.sleep(2000);
-		sds.setSearchColumns();
-	}
-	@Test
+	@Test(priority = 2, dependsOnMethods = "createNewSkill")
 	public void updateSkill() throws InterruptedException, EncryptedDocumentException, IOException {
 		HomePage hp=new HomePage(driver);
 		OrganizationUnitDropDown oudd= new OrganizationUnitDropDown(driver);
@@ -100,13 +46,70 @@ public class skillSetUpTestScript extends com.AutomationJiviewsGeneric.BaseClass
 		hp.setOrgUnit();
 		Thread.sleep(1000);
 		oudd.setOLMop();
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		jmmi.clickOnSystemDefination();
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		esp.setCreateSkill();
 
 		SystemDefinationSkillSetupPage sds=new SystemDefinationSkillSetupPage(driver);
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		sds.setUpdateSkill();
+	}
+
+
+	@Test(priority = 3, dependsOnMethods = {"createNewSkill","updateSkill"})
+	public void deActiveSkill() throws InterruptedException, EncryptedDocumentException, IOException {
+		HomePage hp=new HomePage(driver);
+		OrganizationUnitDropDown oudd= new OrganizationUnitDropDown(driver);
+		jiviewsMainMenuItems jmmi= new jiviewsMainMenuItems(driver);
+		EmployeeSetupPage esp=new EmployeeSetupPage(driver);
+		Thread.sleep(2000);
+		hp.setOrgUnit();
+		Thread.sleep(1000);
+		oudd.setOLMop();
+		Thread.sleep(1000);
+		jmmi.clickOnSystemDefination();
+		Thread.sleep(1000);
+		esp.setCreateSkill();
+
+		SystemDefinationSkillSetupPage sds=new SystemDefinationSkillSetupPage(driver);
+		Thread.sleep(1000);
+		sds.setDeactiveSkil();
+	}
+	@Test(priority = 4, dependsOnMethods = {"createNewSkill","updateSkill","deActiveSkill"})
+	public void setReActiveSkill() throws InterruptedException {
+		HomePage hp=new HomePage(driver);
+		OrganizationUnitDropDown oudd= new OrganizationUnitDropDown(driver);
+		jiviewsMainMenuItems jmmi= new jiviewsMainMenuItems(driver);
+		EmployeeSetupPage esp=new EmployeeSetupPage(driver);
+		SystemDefinationSkillSetupPage sds=new SystemDefinationSkillSetupPage(driver);
+		Thread.sleep(1000);
+		hp.setOrgUnit();
+		Thread.sleep(1000);
+		oudd.setOLMop();
+		Thread.sleep(1000);
+		jmmi.clickOnSystemDefination();
+		Thread.sleep(1000);
+		esp.setCreateSkill();
+		Thread.sleep(1000);
+		sds.setActivateDeactiveSkill();
+	}
+	@Test(priority = 5,dependsOnMethods = {"createNewSkill", "updateSkill", "deActiveSkill", "setReActiveSkill"})
+	public void searchColumns() throws InterruptedException, EncryptedDocumentException, IOException {
+		HomePage hp=new HomePage(driver);
+		OrganizationUnitDropDown oudd= new OrganizationUnitDropDown(driver);
+		jiviewsMainMenuItems jmmi= new jiviewsMainMenuItems(driver);
+		EmployeeSetupPage esp=new EmployeeSetupPage(driver);
+		SystemDefinationSkillSetupPage sds=new SystemDefinationSkillSetupPage(driver);
+		Thread.sleep(1000);
+		hp.setOrgUnit();
+		Thread.sleep(1000);
+		oudd.setOLMop();
+		Thread.sleep(1000);
+		jmmi.clickOnSystemDefination();
+		Thread.sleep(1000);
+		esp.setCreateSkill();
+		Thread.sleep(1000);
+		sds.setSearchColumns();
 	}
 }
