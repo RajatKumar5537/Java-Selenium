7f8bb85a72759409dd6d79c3feb0232f15baecd0
SukruthaJiVi <sukrutha@jivisolutions.com>
2024-02-07 01:09:40 +0530

    first commit

--- src/test/java/com/JiviewsAutomation/SystemDefination_Test/Maritime_Setup_Test.java
+++ src/test/java/com/JiviewsAutomation/SystemDefination_Test/Maritime_Setup_Test.java
@@ -1,88 +1,80 @@
 package com.JiviewsAutomation.SystemDefination_Test;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.testng.Reporter;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
-import com.AutomationJiviewsGeneric.BaseClass;
-import com.AutomationJiviewsGeneric.configUtility;
-import com.AutomationJiviewsPOM.E10_2909_EmpAdmRosterGroupAssignmentPage;
-import com.AutomationJiviewsPOM.EmployeeAdminstrationPage;
-import com.AutomationJiviewsPOM.HomePage;
-import com.AutomationJiviewsPOM.Maritime_Setup;
-import com.AutomationJiviewsPOM.OrganizationUnitDropDown;
-import com.AutomationJiviewsPOM.jiviewsMainMenuItems;
-import com.aventstack.extentreports.model.Report;
+	import org.testng.annotations.Test;
+	import com.AutomationJiviewsGeneric.BaseClass;
+	import com.AutomationJiviewsPOM.HomePage;
+	import com.AutomationJiviewsPOM.Maritime_Setup;
+	import com.AutomationJiviewsPOM.OrganizationUnitDropDown;
+	import com.AutomationJiviewsPOM.jiviewsMainMenuItems;
 
-public class Maritime_Setup_Test extends BaseClass {
-	private static final Logger logger = LogManager.getLogger(E10_2909_EmpAdmRosterGroupAssignmentTest.class);
-	private HomePage homePage;
-	private OrganizationUnitDropDown orgUnit;
-	private jiviewsMainMenuItems jmMenuItem;
-	private EmployeeAdminstrationPage empAdmin;
-	private E10_2909_EmpAdmRosterGroupAssignmentPage rosterGrpAssignment;
-	
-	Maritime_Setup obj;
-
-	@Test(priority = 0)
-	public void setup() throws Exception {
-//		String homeUrl = configUtility.getCongigPropertyData("homeurl");
-//		driver.navigate().to(homeUrl);
-		homePage = new HomePage(driver);
-		orgUnit = new OrganizationUnitDropDown(driver);
-		jmMenuItem = new jiviewsMainMenuItems(driver);
-		homePage.setOrgUnit();
-		orgUnit.setAutoOu();
-		jmMenuItem.clickOnSystemDefination();
+	public class Maritime_Setup_Test extends BaseClass {
+		private HomePage homePage;
+		private OrganizationUnitDropDown orgUnit;
+		private jiviewsMainMenuItems jmMenuItem;
 		
+		Maritime_Setup mariTime;
+		
+		@Test(priority = 1)
+		public void setup() throws Exception {
+			
+			homePage = new HomePage(driver);
+			orgUnit = new OrganizationUnitDropDown(driver);
+			jmMenuItem = new jiviewsMainMenuItems(driver);
+			homePage.setOrgUnit();
+			orgUnit.setAutoOu();
+			jmMenuItem.clickOnSystemDefination();
+			
+			mariTime=new Maritime_Setup(driver);
+			mariTime.maritimeSetup();
+			mariTime.birthSetup();
+		}
 
-	}
-
-	//@Test(priority = 0)
-//	public void clickOnSystemSetup() throws InterruptedException
-//	{
-//		Reporter.log("this is fortest :"+obj, 0);
-//		obj.systemSetup();
-//	}
-//	@Test(priority = 2)
-//	public void clickOnEnterBerthDescription()
-//	{
-//		obj.enterBerthDescription();
-//	}
-	@Test(priority = 1)
-	public void clickOnMaritimeSetup() throws InterruptedException
-	{
-		obj=new Maritime_Setup(driver);
-		obj.maritimeSetup();
-		obj.birthSetup();
-	}
-	//@Test(priority = 4)
-//	public void clickOnBirthSetup()
-//	{
-//		
-//	}
-	@Test(priority = 2)
-	public void verifyCreateBerth()
-	{
-		obj.createBerth();
-	}
-	@Test(priority = 3)
-	public void verifyEditBerth()
-	{
-		obj.editBerth();
-	}
-	@Test(priority = 4)
-	public void verifyDeleteBerth()
-	{
-		obj.deleteBerth();
-	}
-	@Test(priority = 5)
-	public void verifyReActivate()
-	{
-		obj.reActivate();
-	}
-	
-	
+		
+		@Test(priority = 2)
+		public void verifyCreateBerth() throws Exception
+		{
+			mariTime.createBerth();
+		}
+		@Test(priority = 3)
+		public void verifyEditBerth() throws Exception
+		{
+			mariTime.editBerth();
+		}
+		@Test(priority = 4)
+		public void verifyDeleteBerth() throws Exception
+		{
+			mariTime.deleteBerth();
+		}
+		@Test(priority = 5)
+		public void verifyReActivate()throws Exception
+		{
+			mariTime.reActivate();
+		}
+		
+		
+		
+		//@Test(priority = 0)
+//		public void clickOnSystemSetup() throws InterruptedException
+//		{
+//			Reporter.log("this is fortest :"+mariTime, 0);
+//			mariTime.systemSetup();
+//		}
+//		@Test(priority = 2)
+//		public void clickOnEnterBerthDescription()
+//		{
+//			mariTime.enterBerthDescription();
+//		}
+//		@Test(priority = 1)
+//		public void clickOnMaritimeSetup() throws InterruptedException
+//		{
+//			mariTime=new Maritime_Setup(driver);
+//			mariTime.maritimeSetup();
+//			mariTime.birthSetup();
+//		}
+		//@Test(priority = 4)
+//		public void clickOnBirthSetup()
+//		{
+//			
+//		}
 }
